e59ca6d21088d3d2a6f177af18b3a44ae8e89277
swetha gumpena <swetha.gumpena0607@gmail.com>
2022-11-01 22:26:35 -0500

    feat: setup event logging

--- logging.js
+++ logging.js
@@ -12,7 +12,7 @@
 
 var ENABLE_NETWORK_LOGGING = true; // Controls network logging.
 var ENABLE_CONSOLE_LOGGING = true; // Controls console logging.
-var LOG_VERSION = 'A';             // Labels every entry with version: "A".
+var LOG_VERSION = 'B';             // Labels every entry with version: "B".
 
 // These event types are intercepted for logging before jQuery handlers.
 var EVENT_TYPES_TO_LOG = {
@@ -28,125 +28,125 @@ var EVENT_PROPERTIES_TO_LOG = {
 };
 
 // This function is called to record some global state on each event.
-var GLOBAL_STATE_TO_LOG = function() {
+var GLOBAL_STATE_TO_LOG = function () {
   return {
   };
 };
 
-(function() {
+(function () {
 
-// A persistent unique id for the user.
-var uid = getUniqueId();
+  // A persistent unique id for the user.
+  var uid = getUniqueId();
 
-// Hooks up all the event listeners.
-function hookEventsToLog() {
-  // Set up low-level event capturing.  This intercepts all
-  // native events before they bubble, so we log the state
-  // *before* normal event processing.
-  for (var event_type in EVENT_TYPES_TO_LOG) {
-    document.addEventListener(event_type, logEvent, true);
-  }
-
-  // Once the page is loaded, show our own unique id.
-  $(function() {
-    console.log('Your unique id is', uid);
-    $('#bottomtext').html('Logging to the network as <nobr>' + uid + '</nobr>')
-  });
-
-  // Listen to 'log' events which are triggered anywhere in the document.
-  $(document).on('log', logEvent);
-}
-
-// Returns a CSS selector that is descriptive of
-// the element, for example, "td.left div" for
-// a class-less div within a td of class "left".
-function elementDesc(elt) {
-  if (elt == document) {
-    return 'document';
-  } else if (elt == window) {
-    return 'window';
-  }
-  function descArray(elt) {
-    var desc = [elt.tagName.toLowerCase()];
-    if (elt.id) {
-      desc.push('#' + elt.id);
+  // Hooks up all the event listeners.
+  function hookEventsToLog() {
+    // Set up low-level event capturing.  This intercepts all
+    // native events before they bubble, so we log the state
+    // *before* normal event processing.
+    for (var event_type in EVENT_TYPES_TO_LOG) {
+      document.addEventListener(event_type, logEvent, true);
     }
-    for (var j = 0; j < elt.classList.length; j++) {
-      desc.push('.' + elt.classList[j]);
-    }
-    return desc;
-  }
-  var desc = [];
-  while (elt && desc.length <= 1) {
-    var desc2 = descArray(elt);
-    if (desc.length == 0) {
-      desc = desc2;
-    } else if (desc2.length > 1) {
-      desc2.push(' ', desc[0]);
-      desc = desc2;
-    }
-    elt = elt.parentElement;
-  }
-  return desc.join('');
-}
 
-// Parse user agent string by looking for recognized substring.
-function findFirstString(str, choices) {
-  for (var j = 0; j < choices.length; j++) {
-    if (str.indexOf(choices[j]) >= 0) {
-      return choices[j];
+    // Once the page is loaded, show our own unique id.
+    $(function () {
+      console.log('Your unique id is', uid);
+      $('#bottomtext').html('Logging to the network as <nobr>' + uid + '</nobr>')
+    });
+
+    // Listen to 'log' events which are triggered anywhere in the document.
+    $(document).on('log', logEvent);
+  }
+
+  // Returns a CSS selector that is descriptive of
+  // the element, for example, "td.left div" for
+  // a class-less div within a td of class "left".
+  function elementDesc(elt) {
+    if (elt == document) {
+      return 'document';
+    } else if (elt == window) {
+      return 'window';
+    }
+    function descArray(elt) {
+      var desc = [elt.tagName.toLowerCase()];
+      if (elt.id) {
+        desc.push('#' + elt.id);
+      }
+      for (var j = 0; j < elt.classList.length; j++) {
+        desc.push('.' + elt.classList[j]);
+      }
+      return desc;
+    }
+    var desc = [];
+    while (elt && desc.length <= 1) {
+      var desc2 = descArray(elt);
+      if (desc.length == 0) {
+        desc = desc2;
+      } else if (desc2.length > 1) {
+        desc2.push(' ', desc[0]);
+        desc = desc2;
+      }
+      elt = elt.parentElement;
+    }
+    return desc.join('');
+  }
+
+  // Parse user agent string by looking for recognized substring.
+  function findFirstString(str, choices) {
+    for (var j = 0; j < choices.length; j++) {
+      if (str.indexOf(choices[j]) >= 0) {
+        return choices[j];
+      }
+    }
+    return '?';
+  }
+
+  // Genrates or remembers a somewhat-unique ID with distilled user-agent info.
+  function getUniqueId() {
+    if (!('uid' in localStorage)) {
+      var browser = findFirstString(navigator.userAgent, [
+        'Seamonkey', 'Firefox', 'Chromium', 'Chrome', 'Safari', 'OPR', 'Opera',
+        'Edge', 'MSIE', 'Blink', 'Webkit', 'Gecko', 'Trident', 'Mozilla']);
+      var os = findFirstString(navigator.userAgent, [
+        'Android', 'iOS', 'Symbian', 'Blackberry', 'Windows Phone', 'Windows',
+        'OS X', 'Linux', 'iOS', 'CrOS']).replace(/ /g, '_');
+      var unique = ('' + Math.random()).substr(2);
+      localStorage['uid'] = os + '-' + browser + '-' + unique;
+    }
+    return localStorage['uid'];
+  }
+
+  // Log the given event.
+  function logEvent(event, customName, customInfo) {
+    var time = (new Date).getTime();
+    var name = customName || event.type;
+    // By default, monitor some global state on every event.
+    var infoObj = GLOBAL_STATE_TO_LOG();
+    // And monitor a few interesting fields from the event, if present.
+    for (var key in EVENT_PROPERTIES_TO_LOG) {
+      if (key in event) {
+        infoObj[key] = event[key];
+      }
+    }
+    // Let a custom event add fields to the info.
+    if (customInfo) {
+      $.extend(infoObj, customInfo);
+    }
+    var info = JSON.stringify(infoObj);
+    var target = elementDesc(event.target);
+    var state = location.hash;
+
+    if (ENABLE_CONSOLE_LOGGING) {
+      console.log(uid, time, name, target, info, state, LOG_VERSION);
+    }
+    if (ENABLE_NETWORK_LOGGING) {
+      sendNetworkLog(uid, time, name, target, info, state, LOG_VERSION);
     }
   }
-  return '?';
-}
 
-// Genrates or remembers a somewhat-unique ID with distilled user-agent info.
-function getUniqueId() {
-  if (!('uid' in localStorage)) {
-    var browser = findFirstString(navigator.userAgent, [
-      'Seamonkey', 'Firefox', 'Chromium', 'Chrome', 'Safari', 'OPR', 'Opera',
-      'Edge', 'MSIE', 'Blink', 'Webkit', 'Gecko', 'Trident', 'Mozilla']);
-    var os = findFirstString(navigator.userAgent, [
-      'Android', 'iOS', 'Symbian', 'Blackberry', 'Windows Phone', 'Windows',
-      'OS X', 'Linux', 'iOS', 'CrOS']).replace(/ /g, '_');
-    var unique = ('' + Math.random()).substr(2);
-    localStorage['uid'] = os + '-' + browser + '-' + unique;
-  }
-  return localStorage['uid'];
-}
-
-// Log the given event.
-function logEvent(event, customName, customInfo) {
-  var time = (new Date).getTime();
-  var name = customName || event.type;
-  // By default, monitor some global state on every event.
-  var infoObj = GLOBAL_STATE_TO_LOG();
-  // And monitor a few interesting fields from the event, if present.
-  for (var key in EVENT_PROPERTIES_TO_LOG) {
-    if (key in event) {
-      infoObj[key] = event[key];
-    }
-  }
-  // Let a custom event add fields to the info.
-  if (customInfo) {
-    $.extend(infoObj, customInfo);
-  }
-  var info = JSON.stringify(infoObj);
-  var target = elementDesc(event.target);
-  var state = location.hash;
-
-  if (ENABLE_CONSOLE_LOGGING) {
-    console.log(uid, time, name, target, info, state, LOG_VERSION);
-  }
+  // OK, go.
   if (ENABLE_NETWORK_LOGGING) {
-    sendNetworkLog(uid, time, name, target, info, state, LOG_VERSION);
+    hookEventsToLog();
   }
-}
-
-// OK, go.
-if (ENABLE_NETWORK_LOGGING) {
-  hookEventsToLog();
-}
 
 })();
 
@@ -177,24 +177,24 @@ if (ENABLE_NETWORK_LOGGING) {
 
 // "A" version network log submission function
 // submits to the google form at this URL:
-// docs.google.com/forms/d/1Ao......................................QE0/edit
+// docs.google.com/forms/d/e/1FAIpQLSczlbq3ODdyVmMFhn6eek1DU9xx3m9JnPHACAS7sTCDenbLTA/viewform?usp=sf_link
 function sendNetworkLog(
-    uid,
-    time,
-    name,
-    target,
-    info,
-    state,
-    version) {
-  var formid = "e/1FA==--FIX-ME-BY-SETTING-UP-LOGGING-JS--==NndeQ";
+  uid,
+  time,
+  name,
+  target,
+  info,
+  state,
+  version) {
+  var formid = "e/1FAIpQLSczlbq3ODdyVmMFhn6eek1DU9xx3m9JnPHACAS7sTCDenbLTA";
   var data = {
-    "entry.1291686978": uid,
-    "entry.211470577": time,
-    "entry.293924811": name,
-    "entry.1498801356": target,
-    "entry.1636492479": info,
-    "entry.22711366": state,
-    "entry.1740074722": version
+    "entry.802102400": uid,
+    "entry.1872854758": time,
+    "entry.278772069": name,
+    "entry.1235007535": target,
+    "entry.1899566398": info,
+    "entry.1729873352": state,
+    "entry.1206394851": version
   };
   var params = [];
   for (key in data) {
@@ -202,5 +202,5 @@ function sendNetworkLog(
   }
   // Submit the form using an image to avoid CORS warnings.
   (new Image).src = "https://docs.google.com/forms/d/" + formid +
-     "/formResponse?" + params.join("&");
+    "/formResponse?" + params.join("&");
 }
